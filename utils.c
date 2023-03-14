183dd1dbfab32cb8e2c59d71dacaa87451eddaee
nokosse <visouthivong.kevin@gmail.com>
2023-03-14 13:43:26 +0100

    reset

--- utils.c
+++ utils.c
@@ -6,55 +6,8 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 15:06:09 by kvisouth          #+#    #+#             */
-/*   Updated: 2023/03/14 13:38:25 by kvisouth         ###   ########.fr       */
+/*   Updated: 2023/03/14 13:43:08 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
-
-int	is_str_numeric(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-	{
-		if (!ft_isdigit(str[i]))
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
-char	*str_append_c_first(char c)
-{
-	char	*add;
-
-	add = (char *)malloc(sizeof(char) * 2);
-	if (!add)
-		return (NULL);
-	add[0] = c;
-	add[1] = '\0';
-	return (add);
-}
-
-char	*str_append_c(char *str, char c)
-{
-	char	*add;
-	int		i;
-
-	if (!c)
-		return (NULL);
-	if (!str)
-		return (str_append_c_first(c));
-	add = (char *)malloc(sizeof(char) * (ft_strlen(str) + 2));
-	if (!add)
-		return (free(str), NULL);
-	i = -1;
-	while (str[++i])
-		add[i] = str[i];
-	free(str);
-	add[i++] = c;
-	add[i] = '\0';
-	return (add);
-}
