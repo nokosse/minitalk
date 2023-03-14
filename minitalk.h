51882d78cdce82d44f84d4a9f36b90c09045e3d8
nokosse <visouthivong.kevin@gmail.com>
2023-03-14 13:40:30 +0100

    norms

--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,7 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 13:16:34 by kvisouth          #+#    #+#             */
-/*   Updated: 2023/03/14 13:40:08 by kvisouth         ###   ########.fr       */
+/*   Updated: 2023/03/14 13:40:25 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 # include <signal.h>
 # include "libft/inc/libft.h"
-# sinclude "libft/inc/ft_printf.h"
+# include "libft/inc/ft_printf.h"
 
 int		is_str_numeric(char *str);
 char	*str_append_c(char *str, char c);
