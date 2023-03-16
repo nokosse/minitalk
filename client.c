803eb93978c7b0db250c94aebc447b836042d9f2
nokosse <visouthivong.kevin@gmail.com>
2023-03-16 19:06:22 +0100

    working in Binary

--- client.c
+++ client.c
@@ -6,44 +6,105 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 13:15:10 by kvisouth          #+#    #+#             */
-/*   Updated: 2023/03/16 13:31:19 by kvisouth         ###   ########.fr       */
+/*   Updated: 2023/03/16 18:24:50 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-// How does minitalk will work ?
-// The goal : send a message (string of ASCII characters) from client to server.
-// - The two programs can ONLY communicate by using SIGUSR1 and SIGUSR2 signals.
-// - An ASCII character is 8 bits long. For example, the letter A is 01000001.
-// - When the client send "ABC" to the senver, it will send 8*3 = 24 signals.
-// - The first 8 signals represent the 'A', the 8 next the 'B' and so on.
-// - To send 01000001, the client will send :
-// - SIGUSR2 (0)
-// - SIGUSR1 (1)
-// - SIGUSR2 (0)
-// - SIGUSR2 (0)
-// - SIGUSR2 (0)
-// - SIGUSR2 (0)
-// - SIGUSR1 (1)
-// - SIGUSR1 (1)
-// - The server will do nothing until it recieves 8 signals.
-// - At every signal, the server will store the binary value of the signal.
-// - Server will send SIGUSR1 to client if everying is ok.
-// - If something failed, the server will send SIGUSR2 to client.
-// - When the server recieves 8 signals, it will convert the binary value
-//   we stored into a decimal value. In this case, 01000001 = 65.
-//   65 is the ASCII code of the letter A.
-// - Then print this ASCII code on the screen with write().
-//
-// - The client repeats this process for 'B' and 'C' and for '\0'.
-// - If the server recieves 8 SIGUSR2 signals, it means that the client
-//   sent the '\0' character, meaning that the message is over.
-// - The client program will stop sending signals and the server will
-//   idle until it recieves a new message.
-//
-// To make it clear :
-// CLIENT --> SERVER    SERVER --> CLIENT
-// SIGUSR1 = 1          SIGUSR1 = I got your message
-// SIGUSR2 = 0          SIGUSR2 = I didn't get your message
+// This function will send 8 bits of '0' to the server.
+// 00000000 (\0), meaning the end of the string.
+int	send_end(int pid)
+{
+	int	i;
 
+	i = 0;
+	while (i < 8)
+	{
+		if (kill(pid, SIGUSR2) == -1)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+// This function will convert the string to binary.
+// ABC = 011000010110001001100011
+// bin = 011000010110001001100011\0
+char	*str_to_bin(char *str)
+{
+	char	*bin;
+	int		i;
+	int		j;
+	int		k;
+
+	bin = malloc(sizeof(char) * (8 * ft_strlen(str)) + 1);
+	if (!bin)
+		return (NULL);
+	i = 0;
+	while (*str)
+	{
+		j = 7;
+		while (j >= 0)
+		{
+			k = *str >> j;
+			if (k & 1)
+				bin[i] = '1';
+			else
+				bin[i] = '0';
+			i++;
+			j--;
+		}
+		str++;
+	}
+	return (bin[i] = '\0', bin);
+}
+
+// This function will send the binary to the server.
+// It will parse *bin and send SIGUSR1 on 1 and SIGUSR2 on 0.
+int	send_signal(int pid, char *bin)
+{
+	int	i;
+
+	i = 0;
+	while (bin[i])
+	{
+		if (kill(pid, 0) == -1)
+			return (0);
+		if (bin[i] == '1')
+		{
+			printf("Sending SIGUSR1 to %d\n", pid);
+			if (kill(pid, SIGUSR1) == -1)
+				return (0);
+		}
+		else if (bin[i] == '0')
+		{
+			printf("Sending SIGUSR2 to %d\n", pid);
+			if (kill(pid, SIGUSR2) == -1)
+				return (0);
+		}
+		usleep(200);
+		i++;
+	}
+	// send_end(pid);
+	if (kill(pid, 0) == -1)
+		return (0);
+	return (1);
+}
+
+int	main(int ac, char **av)
+{
+	char	*bin;
+	int		pid;
+
+	if (ac != 3)
+	{
+		ft_putstr_fd("client : invalid arguments\n", 2);
+		exit(EXIT_FAILURE);
+	}
+	pid = ft_atoi(av[1]);
+	bin = str_to_bin(av[2]);
+	send_signal(pid, bin);
+	free(bin);
+	return (0);
+}
--- server.c
+++ server.c
@@ -6,9 +6,70 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 13:20:56 by kvisouth          #+#    #+#             */
-/*   Updated: 2023/03/16 13:31:22 by kvisouth         ###   ########.fr       */
+/*   Updated: 2023/03/16 18:57:43 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+// How does minitalk (server) will work ?
+// It will 
+
+// This function 
+char	*get_bin(int num)
+{
+	static char	bin[9] = {'\0'};
+	int			i;
+
+	i = 7;
+	while (i >= 0)
+	{
+		bin[i] = ((num & 1) ^ 1) + '0';
+		num >>= 1;
+		i--;
+	}
+	return (bin);
+}
+
+void	handle_signal(int sig)
+{
+	static int	count = 0;
+	static int	num = 0;
+
+	if (sig == SIGUSR1)
+	{
+		num <<= 1;
+		count++;
+	}
+	else if (sig == SIGUSR2)
+	{
+		num <<= 1;
+		num |= 1;
+		count++;
+	}
+	if (count == 8)
+	{
+		printf("%s\n", get_bin(num));
+		count = 0;
+		num = 0;
+	}
+}
+
+int	main(void)
+{
+	pid_t				pid;
+	struct	sigaction	sa;
+
+	pid = getpid();
+	printf("PID: %d\n", pid);
+	
+	sa.sa_handler = handle_signal;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	
+	while (1)
+		pause();
+	return (0);
+}
